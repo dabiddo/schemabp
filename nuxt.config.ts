3d775eb539bc63f781b18d32eaee96116e0509a8
David Torres <dabiddo@gmail.com>
2024-12-30 20:15:40 +0000

    added jetbrains-mono as webfont

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    '~/assets/css/main.css'
+  ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700&display=swap',
+        }
+      ]
+    }
+  }
 })
